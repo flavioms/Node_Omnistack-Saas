141051c1b33aa001febd8ab8e1da3084da2a307c
Flavio Martins <flaviomartins_silva@hotmail.com>
2019-09-27 19:45:18 -0300

    feature/Add Seed for User and Team

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -11,10 +11,20 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-const Factory = use('Factory')
+const User = use('App/Models/User')
 
 class DatabaseSeeder {
   async run () {
+    const user = await User.create({
+      name: 'Flavio Martins',
+      email: 'flavio@teste.com.br',
+      password: '123456'
+    })
+
+    await user.teams().create({
+      name: 'Rocketseat',
+      user_id: user.id
+    })
   }
 }
 
